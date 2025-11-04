95a61833ce2422ecec9031ce9a4fd4ab53894a74
SolyomSzabi <solymszabi@gmail.com>
2025-11-04 22:54:44 +0000

    auto-commit for 3dd63238-fb15-4e88-b6b3-a2e434ea5039

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -134,7 +134,7 @@ const Services = () => {
                             </div>
                             <div className="flex items-center space-x-1">
                               <DollarSign className="h-4 w-4 text-zinc-400" />
-                              <span className="text-lg font-bold text-yellow-600">From ${service.base_price}*</span>
+                              <span className="text-lg font-bold text-yellow-600">De la {service.base_price} RON*</span>
                             </div>
                           </div>
                         </div>
